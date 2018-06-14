9118c260c5a1900fe6587b296f69123748ef98d3
阮超 <ruanchao@shuwen.com>
2018-06-14 12:22:23 +0800

    [1]

---
+++ main-hook-app2/src/main/java/com/ruanchao/app2/SynActivity.java
@@ -0,0 +1,14 @@
+package com.ruanchao.app2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SynActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_syn);
+        findViewById(R.id.et_bg_music_path);
+    }
+}
